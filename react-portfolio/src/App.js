6c81d4ff4729add4bd54a6f99fa78f9758300373
el bandingo <elbandingo@protonmail.com>
2022-03-05 17:00:52 -0330

    created the components folder, assigned header and footer in the App.js file as they are going to be default rendered everytime

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>testing the removal of default app</h1>
-    </div>
+<div>
+    <Header></Header>
+    <Footer></Footer>
+</div>
   );
 }
 
