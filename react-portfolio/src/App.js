46a54d63fc75d9cd177dac64221c4326130d4ee1
el bandingo <elbandingo@protonmail.com>
2022-03-05 17:59:12 -0330

    basic footer has been updated, will style and add links later

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 function App() {
   return (
 <div>
 <Header></Header>
+<Footer></Footer>
 </div>
   );
 }
---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+//declare function for footer to return informaiton to my profiles
+function Footer() {
+    return(
+    <footer className="footer">
+        <div className="content has-text-centered">
+            <p>
+                <b>Adam Lewis - Full Stack Web Development Portfolio 2022</b> - a React site by Adam Lewis
+            </p>
+        </div>
+    </footer>
+    );
+}
+
+export default Footer;
