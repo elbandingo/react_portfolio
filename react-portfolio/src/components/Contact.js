3446e86cc404cf5e76104dc54fa4879b0c54f47e
el bandingo <elbandingo@protonmail.com>
2022-03-05 19:41:35 -0330

    form touch up, moving on to resume

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -20,7 +20,7 @@ function Contact() {
 
     //function for handling changes in form, validating emails, and ensuring fields are filled out
     const formHandler = (e) => {
-    if(e.target.name === 'email') {
+    if(e.target.name === email) {
         const isValid = validateEmail(e.target.value);
         if(!isValid) {
             setErrMessage('that is an invalid email address');
