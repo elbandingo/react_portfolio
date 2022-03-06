4be97bf2b42df2144d26eb597fa8a3e1c6a3912f
el bandingo <elbandingo@protonmail.com>
2022-03-05 20:53:18 -0330

    added the projects component, and portfolio. built an object to handle the contents for the cards that get displayed

---
+++ react-portfolio/src/components/Portfolio.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Project from "./Project";
+
+//declare an array of objects called projects which contains all the api data for my portfolio
+const projects = [
+    {
+        id: 0,
+        title: "Run Buddy",
+        image: "/img/rbuddy.jpg",
+        description: "This was my first code along project for the Web Development Bootcamp. This focused on core elements of HTML and CSS",
+        github: "https://github.com/elbandingo/rbuddy2",
+        live: "https://elbandingo.github.io/rbuddy2/"
+
+    },
+
+    {
+        id: 1,
+        title: "On-Demand-CRM",
+        image: "/img/crm.jpg",
+        description: "This group project was a collaboration to create a client information CRM application",
+        github: "https://github.com/AhmedAlkh/On-Demand-CRM",
+        live: "https://mysterious-badlands-54283.herokuapp.com/"
+    },
+
+    {
+        id: 2,
+        title: "Weather Dashboard",
+        image: "/img/weather.jpg",
+        description: "This application was built to check the weather of any city registered on the openWeather API",
+        github: "https://github.com/elbandingo/weather_dashboard",
+        live: "https://elbandingo.github.io/weather_dashboard/"
+    },
+
+    {
+        id: 3,
+        title: "Password Generator",
+        image: "/img/password.jpg",
+        description: "This application lets you create a secure password with the ability to include combinations of specific characters and types",
+        github: "https://github.com/elbandingo/passwordGenerator",
+        live: "https://elbandingo.github.io/passwordGenerator/"
+    }
+
+]
+
+
+
+function Portfolio() {
+    return (
+        <div>
+            <p>Portfolio</p>
+            <hr />
+            <Project projects={projects} />
+        </div>
+    );
+}
+
+export default Portfolio;
---
+++ react-portfolio/src/components/Project.js
@@ -0,0 +1,22 @@
+//creates the HTML content based on the object i have of my portfolio
+import React from "react";
+
+function Project(props) {
+return(
+    <div className="row">
+        {props.projects.map((project) => (
+            <div className="col-md-6 mt-4">
+                <div className="card" style={{width: '25rem',}}>
+                <img src={process.env.PUBLIC_URL + project.image} className="card-img-top" style={{objectFit:'cover', width: '100%', height: '20vw'}} alt="#" />
+                <h5 className="text-center">{project.title}</h5>
+                <p className="card-text">{project.description}</p>
+                <a href={project.github} className="btn btn-primary" target="_blank">to GitHub!</a>
+                <a href={project.live} className="btn btn-primary" target="_blank">Deployed Site!</a>
+                </div>
+            </div>
+        ))}
+    </div>
+);
+}
+
+export default Project;
